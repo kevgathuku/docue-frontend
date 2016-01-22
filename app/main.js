839a7271a36059bd5c8c8ccdceec0ec8ad051bef
Kevin Ndung'u <kevgathuku@gmail.com>
2016-01-22 14:51:48 +0300

    [Starts #112206745] Add react-router integration

--- app/main.js
+++ app/main.js
@@ -1,9 +1,19 @@
 (() => {
   'use strict';
 
-  let React = require('react');
-  let ReactDOM = require('react-dom');
-  let Index = require('./components/Landing/index.jsx');
+  let React = require('react'),
+      ReactDOM = require('react-dom'),
+      ReactRouter = require('react-router'),
+      IndexRoute = ReactRouter.IndexRoute,
+      Route = ReactRouter.Route,
+      Router = ReactRouter.Router,
+      browserHistory = ReactRouter.browserHistory,
+      Landing = require('./components/Landing/index.jsx');
 
-  ReactDOM.render(<Index /> , document.getElementById('content'));
+  ReactDOM.render((
+    <Router history={browserHistory}>
+      <Route path="/" component={Landing} >
+        <IndexRoute component={Landing} />
+      </Route>
+    </Router>) , document.getElementById('content'));
 })();
