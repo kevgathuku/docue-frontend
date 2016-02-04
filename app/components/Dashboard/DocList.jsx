472f68b4ff03247f916beec128e00e84ac152dd2
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2016-02-04 14:35:25 +0300

    [#112659885] Add delete document dialog
    Show a sweetalert dialog when a doc is deleted

--- app/components/Dashboard/DocList.jsx
+++ app/components/Dashboard/DocList.jsx
@@ -10,6 +10,8 @@
 
     constructor(props) {
       super(props);
+
+      this.handleDeleteClick = this.handleDeleteClick.bind(this);
     }
 
     componentDidMount() {
@@ -19,31 +21,62 @@
       });
     }
 
+    handleDeleteClick(event) {
+      // Prevent the default action for clicking on a link
+      event.preventDefault();
+      window.swal({
+        title: 'Are you sure?',
+        text: 'You will not be able to recover this document!',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#DD6B55',
+        confirmButtonText: 'Yes, delete it!',
+        closeOnConfirm: false
+        }, () => {
+          window.swal('Deleted!', 'Your document has been deleted.', 'success');
+        });
+    }
+
     render() {
       let renderDoc = function(doc) {
         return (
           <div className="col s12 m6 l4" key={doc._id}>
             <div className="card">
               <div className="card-image">
-                <img src="http://lorempixel.com/484/363/" />
+                <img src="http://lorempixel.com/484/363/"/>
               </div>
               <div className="card-content">
                 <h5>{doc.title}</h5>
-                <p>Creator: {`${doc.ownerId.name.first} ${doc.ownerId.name.last}`}</p>
+                <p>Creator:
+                  {`${doc.ownerId.name.first} ${doc.ownerId.name.last}`}</p>
               </div>
               <div className="card-action">
                 <a className="tooltipped" data-position="top" data-delay="50" data-tooltip="Details">
-                  <i className="material-icons">info</i></a>
-                <a className="tooltipped" data-position="top" data-delay="50" data-tooltip="Edit">
-                  <i className="material-icons">mode_edit</i></a>
-                <a className="tooltipped" data-position="top" data-delay="50" data-tooltip="Delete">
-                  <i className="material-icons">delete</i></a>
+                  <i className="material-icons">info</i>
+                </a>
+                <a className="tooltipped"
+                    data-position="top"
+                    data-delay="50"
+                    data-tooltip="Edit"
+                >
+                  <i className="material-icons">mode_edit</i>
+                </a>
+                <a className="modal-trigger tooltipped"
+                    data-position="top"
+                    data-delay="50"
+                    data-tooltip="Delete"
+                    onClick={this.handleDeleteClick}
+                >
+                  <i className="material-icons">delete</i>
+                </a>
               </div>
             </div>
           </div>
         );
       };
-      return (<div>{this.props.docs.map(renderDoc)}</div>);
+      return (
+        <div>{this.props.docs.map(renderDoc, this)}</div>
+      );
     }
   }
   module.exports = DocList;
