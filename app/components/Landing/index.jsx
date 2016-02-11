95a127be9e87004227d369de28fff16dcab0d0ff
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2016-02-11 18:27:21 +0300

    [#113076247] Refactor Landing Page

--- app/components/Landing/index.jsx
+++ app/components/Landing/index.jsx
@@ -1,7 +1,6 @@
 (function() {
   'use strict';
   let React = require('react');
-  let Home = require('./Home.jsx');
 
   // Requires the CSS for it to be included in the final output
   require('../../styles/select.css');
@@ -14,12 +13,33 @@
 
     render() {
       return (
-        <div>
-          <Home/>
+        <div id="hero">
+          <div className="container" id="hero-text-container">
+            <div className="row">
+              <div className="col s12 center-align">
+                <h1 id="hero-title" itemProp="description">
+                  <span className="bold" >{'Docue    '}</span>
+                  <span className="thin">
+                    is the simplest way for <br />
+                  anyone to manage their documents online </span>
+                </h1>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col s12">
+                <div className="center-align">
+                  <a className="btn btn-large create-list-link hero-btn" href="/auth"> Get Started
+                  </a>
+                  <a className="btn btn-large white grey-text hero-btn" href="#">Learn More</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       );
     }
   }
 
   module.exports = Landing;
+
 })();
