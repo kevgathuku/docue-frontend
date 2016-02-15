6a111ce05f912183b01f0d34e6abdd0c8502cfe7
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2016-02-15 14:55:07 +0300

    [Finishes #112872187]
    Display user menu only if the user is logged in

--- app/components/NavBar/index.jsx
+++ app/components/NavBar/index.jsx
@@ -17,6 +17,7 @@
         user: null
       };
       this.userSession = this.userSession.bind(this);
+      this.handleDropdownClick = this.handleDropdownClick.bind(this);
       this.handleLogoutResult = this.handleLogoutResult.bind(this);
       this.handleLogoutSubmit = this.handleLogoutSubmit.bind(this);
     }
@@ -54,6 +55,11 @@
       }
     }
 
+    handleDropdownClick(event) {
+      event.preventDefault();
+      window.$('.dropdown-button').dropdown();
+    }
+
     handleLogoutSubmit(event) {
       event.preventDefault();
       // Send a request to check if the user is logged in
@@ -80,33 +86,36 @@
     render() {
       return (
         <nav className="transparent black-text" role="navigation">
-          <ul id="dropdown" className="dropdown-content">
-            <li><a href="/#">My Profile</a></li>
-            <li><a href="/#">My Documents</a></li>
-            <li className="divider"></li>
-            <li>
-              <a href="/#"
-                  onClick={this.handleLogoutSubmit}
-              > Logout
-              </a>
-            </li>
-          </ul>
           <div className="nav-wrapper container">
             <a className="brand-logo brand-logo-small" href="/">
               <img alt="Docue Logo" id="header-logo" src={logoSrc}/>
-              {'      Docue'}</a>
+              {'      Docue'}
+            </a>
             <ul className="right hide-on-med-and-down" id="nav-mobile">
               <li>
                 {this.state.loggedIn === 'true'
-                  ? <a className="dropdown-button"
-                      data-activates="dropdown"
-                      data-beloworigin="true"
-                      data-constrainwidth="false"
-                      href="/#"
-                    >{this.state.user.name.first}
-                    <i className="material-icons right">arrow_drop_down</i>
-                    </a>
-                  : <a className="dropdown-button" data-activates="dropdown" href="/auth">Login</a>
+                  ? <div>
+                      <ul id="dropdown" className="dropdown-content">
+                        <li><a href="/#">My Profile</a></li>
+                        <li><a href="/#">My Documents</a></li>
+                        <li className="divider"></li>
+                        <li>
+                          <a href="/#"
+                              onClick={this.handleLogoutSubmit}
+                          > Logout
+                          </a>
+                        </li>
+                      </ul>
+                      <a className="dropdown-button"
+                          data-activates="dropdown"
+                          data-beloworigin="true"
+                          data-constrainwidth="false"
+                          onClick={this.handleDropdownClick}
+                      >{this.state.user.name.first}
+                        <i className="material-icons right">arrow_drop_down</i>
+                      </a>
+                    </div>
+                  : <a href="/auth">Login</a>
                 }
               </li>
             </ul>
