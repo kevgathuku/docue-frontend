d76cdee577ada232337f095664e93ed5d12a1642
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2016-02-23 10:07:57 +0300

    [#114043561] Use expect instead of Chai

--- app/components/Landing/__tests__/Landing-test.js
+++ app/components/Landing/__tests__/Landing-test.js
@@ -1,18 +1,18 @@
 'use strict';
 
 import React from 'react';
-import { expect } from 'chai';
+import expect from 'expect';
 import { shallow, mount } from 'enzyme';
 import Landing from '../index.jsx';
 
 describe('Landing component', function() {
   it('renders the correct content', function() {
-    expect(shallow(<Landing />).text()).to.match(/Docue\s+is the simplest way for anyone to manage their documents online/);
-    expect(shallow(<Landing />).text()).to.match(/Get\s+Started/);
+    expect(shallow(<Landing />).text()).toMatch(/Docue\s+is the simplest way for anyone to manage their documents online/);
+    expect(shallow(<Landing />).text()).toMatch(/Get\s+Started/);
   });
 
   it('renders the correct component', function() {
-    expect(shallow(<Landing />).is('#hero')).to.equal(true);
-    expect(mount(<Landing />).find('#hero').length).to.equal(1);
+    expect(shallow(<Landing />).is('#hero')).toEqual(true);
+    expect(mount(<Landing />).find('#hero').length).toEqual(1);
   });
 });
--- app/components/NavBar/__tests__/NavBar-test.js
+++ app/components/NavBar/__tests__/NavBar-test.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { expect } from 'chai';
+import expect from 'expect';
 import { mount, shallow } from 'enzyme';
 import NavBar from '../index.jsx';
 
 describe('NavBar component', function() {
   it('renders the correct content', function() {
     // It should find the correct title
-    expect(shallow(<NavBar />).text()).to.match(/Docue/);
+    expect(shallow(<NavBar />).text()).toMatch(/Docue/);
     // It should render the correct menu items
-    expect(shallow(<NavBar />).text()).to.match(/Home/);
-    expect(shallow(<NavBar />).text()).to.match(/About/);
-    expect(shallow(<NavBar />).text()).to.match(/Login/);
+    expect(shallow(<NavBar />).text()).toMatch(/Home/);
+    expect(shallow(<NavBar />).text()).toMatch(/About/);
+    expect(shallow(<NavBar />).text()).toMatch(/Login/);
   });
 
   it('renders the correct children components', function() {
-    expect(shallow(<NavBar />).is('.transparent')).to.equal(true);
+    expect(shallow(<NavBar />).is('.transparent')).toEqual(true);
     // It should render the site logo
-    expect(mount(<NavBar />).find('img').length).to.equal(1);
+    expect(mount(<NavBar />).find('img').length).toEqual(1);
   });
 });
