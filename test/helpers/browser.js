d29851aaee19b988899f44180a346e648bd6b9f5
Kevin Ndung'u <kevgathuku@gmail.com>
2016-07-12 01:07:55 +0300

    Refactor testing browser environment
    - Use testdom to append more browser globals
    - Disable requiring of non-js files in tests

--- test/helpers/browser.js
+++ test/helpers/browser.js
@@ -1,25 +1,38 @@
-require('babel-register')();
-
-var jsdom = require('jsdom').jsdom;
-var localStorage =  require('localStorage');
-var sessionstorage =  require('sessionstorage');
-
-var exposedProperties = ['window', 'navigator', 'document'];
-
-global.document = jsdom('');
-global.window = document.defaultView;
-global.window.localStorage = localStorage;
-global.window.sessionstorage = sessionstorage;
-
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
+// Attaches extra objects to global object
+require('testdom')('<!doctype html><html><body></body></html>', {
+  localStorage : 'localStorage',
+  sessionStorage : 'sessionstorage'
 });
 
-global.navigator = {
-  userAgent: 'node.js'
+// Attach sessionStorage to the window global
+window.sessionStorage = sessionStorage;
+
+// Tests are placed alongside files under test.
+// This file does the following:
+// 1. Sets the environment to 'production' so that
+//    dev-specific babel config in .babelrc doesn't run.
+// 2. Disables Webpack-specific features that Mocha doesn't understand.
+// 3. Registers babel for transpiling our code for testing.
+
+// This assures the .babelrc dev config (which includes
+// hot module reloading code) doesn't apply for tests.
+process.env.NODE_ENV = 'production';
+
+// Disable webpack-specific features for tests since
+// Mocha doesn't know what to do with them.
+require.extensions['.css'] = function () {
+  return null;
+};
+require.extensions['.png'] = function () {
+  return null;
+};
+require.extensions['.jpg'] = function () {
+  return null;
+};
+require.extensions['.jpeg'] = function () {
+  return null;
 };
 
-documentRef = document;
+// Register babel so that it will transpile ES6 to ES5
+// before our tests run.
+require('babel-register')();
