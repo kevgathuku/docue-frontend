e107ecbe5dd67311b7577a32649571dcfbec18ba
Kevin Ndung'u <kevgathuku@gmail.com>
2017-05-23 11:12:08 +0300

    Add tests for NotFound Elm component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Elm from 'react-elm-components';
-import { Main } from '../NotFound.elm';
+import { NotFound } from '../NotFound.elm';
 
-export default class NotFound extends React.PureComponent {
+export default class Main extends React.PureComponent {
   render() {
-    return <Elm src={Main} />;
+    return <Elm src={NotFound} />;
   }
 }
