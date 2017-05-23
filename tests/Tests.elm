4a349f007a82e3b7f0d0a7c3f1849b4d859dc258
Kevin Ndung'u <kevgathuku@gmail.com>
2017-05-23 12:07:31 +0300

    Port landing page component to Elm

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,28 +2,43 @@ module Tests exposing (..)
 
 import Test exposing (..)
 import Expect
-import NotFound
-import Html
-import Html.Attributes exposing (class)
 import Test.Html.Query as Query
-import Test.Html.Selector exposing (text, tag)
+import Test.Html.Selector exposing (attribute, text, tag)
+import Landing
+import NotFound
 
 
 all : Test
 all =
-    describe "Sample Test Suite"
-        [ describe "Not Found"
-            [ test "Correctly Renders Model Content" <|
-                \() ->
-                    NotFound.view "Nothing"
-                        |> Query.fromHtml
-                        |> Query.find [ tag "h2" ]
-                        |> Query.has [ text "Nothing" ]
-            , test "Renders static text" <|
-                \() ->
-                    NotFound.view "Nothing"
-                        |> Query.fromHtml
-                        |> Query.find [ tag "p" ]
-                        |> Query.has [ text "Sorry. This is not the page you were looking for" ]
-            ]
+    describe "Docue Test Suite"
+        [ notFound, landing ]
+
+
+notFound : Test
+notFound =
+    describe "Not Found"
+        [ test "Correctly Renders Model Content" <|
+            \() ->
+                NotFound.view "Nothing"
+                    |> Query.fromHtml
+                    |> Query.find [ tag "h2" ]
+                    |> Query.has [ text "Nothing" ]
+        , test "Renders static text" <|
+            \() ->
+                NotFound.view "Nothing"
+                    |> Query.fromHtml
+                    |> Query.find [ tag "p" ]
+                    |> Query.has [ text "Sorry. This is not the page you were looking for" ]
+        ]
+
+
+landing : Test
+landing =
+    describe "Landing"
+        [ test "Correctly Renders Model Content" <|
+            \() ->
+                Landing.view { authLink = "/authenticate" }
+                    |> Query.fromHtml
+                    |> Query.find [ attribute "href" "/authenticate" ]
+                    |> Query.has [ text "Get Started" ]
         ]
