a297e663218de1d466ee1d998b2500c5fa15d42a
Kevin Ndung'u <kevgathuku@gmail.com>
2017-07-03 12:57:09 +0300

    Port view part of Login to Elm

---
+++ src/components/Login.elm
@@ -0,0 +1,72 @@
+port module Login exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onSubmit)
+
+
+type alias Model =
+    { email : String
+    , password : String
+    }
+
+
+type Msg
+    = Submit
+    | EmailChange String
+    | PasswordChange String
+
+
+
+-- port for sending strings out to JavaScript
+
+
+port handleSubmit : Model -> Cmd msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Submit ->
+            ( model, handleSubmit model )
+
+        EmailChange value ->
+            ( { model | email = value }, Cmd.none )
+
+        PasswordChange value ->
+            ( { model | password = value }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "row" ]
+        [ Html.form [ class "col s12", onSubmit Submit ]
+            [ div [ class "input-field col s12" ]
+                [ input [ class "validate", id "email", name "email", value model.email, onInput EmailChange, required True, type_ "text" ] []
+                , label [ for "email" ] [ text "Email Address" ]
+                ]
+            , div [ class "input-field col s12" ]
+                [ input [ class "validate", id "password", name "password", value model.password, onInput PasswordChange, required True, type_ "password" ] []
+                , label [ for "password" ] [ text "Password" ]
+                ]
+            , div [ class "col s12" ]
+                [ div [ class "container center" ]
+                    [ button [ class "btn waves-effect header-btn blue", name "action", type_ "submit" ] [ text "Login" ]
+                    ]
+                ]
+            ]
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { email = "", password = "" }, Cmd.none )
+
+
+main =
+    program { init = init, view = view, update = update, subscriptions = subscriptions }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,98 +1,63 @@
 import React from 'react';
-import {observe} from 'mobx';
-import {observer, PropTypes} from 'mobx-react';
-import {browserHistory} from 'react-router';
+import Elm from 'react-elm-components';
+import { observe } from 'mobx';
+import { observer, PropTypes } from 'mobx-react';
+import { browserHistory } from 'react-router';
+import { Login } from '../Login.elm';
 
-import { handleFieldChange } from '../../utils/componentHelpers';
 import UserActions from '../../actions/UserActions';
 
-const LoginForm = observer(class LoginForm extends React.Component {
-  static propTypes = {
-    userStore: PropTypes.observableObject
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.userStore = this.props.userStore;
-    this.state = {
-      email: null,
-      password: null,
-      result: null
+const LoginForm = observer(
+  class LoginForm extends React.Component {
+    static propTypes = {
+      userStore: PropTypes.observableObject
     };
-  }
 
-  componentDidMount() {
-    // Mobx eventListeners
-    observe(this.userStore, 'loginResult', this.handleLoginResult);
-  }
-
-  handleLoginResult = () => {
-    let data = this.userStore.loginResult;
-    if (data) {
-      if (data.error) {
-        window.Materialize.toast(data.error, 2000, 'error-toast');
-      } else {
-        // The login was successful. Store user data in localStorage
-        localStorage.setItem('user', data.token);
-        localStorage.setItem('userInfo', JSON.stringify(data.user));
-        window.Materialize.toast('Logged in Successfully!', 2000, 'success-toast');
-        browserHistory.push('/dashboard');
-      }
+    constructor(props) {
+      super(props);
+      this.userStore = this.props.userStore;
     }
-  };
 
-  handleFieldChange = (event) => {
-    let stateObject = handleFieldChange(event);
-    this.setState(stateObject);
-  };
+    componentDidMount() {
+      // Mobx eventListeners
+      observe(this.userStore, 'loginResult', this.handleLoginResult);
+    }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    let loginPayload = {
-      username: this.state.email,
-      password: this.state.password
+    handleLoginResult = () => {
+      let data = this.userStore.loginResult;
+      if (data) {
+        if (data.error) {
+          window.Materialize.toast(data.error, 2000, 'error-toast');
+        } else {
+          // The login was successful. Store user data in localStorage
+          localStorage.setItem('user', data.token);
+          localStorage.setItem('userInfo', JSON.stringify(data.user));
+          window.Materialize.toast(
+            'Logged in Successfully!',
+            2000,
+            'success-toast'
+          );
+          browserHistory.push('/dashboard');
+        }
+      }
     };
-    UserActions.login(loginPayload, this.userStore);
-  };
 
-  render() {
-    return (
-      <div className="row">
-        <form className="col s12" onSubmit={this.handleSubmit}>
-          <div className="input-field col s12">
-            <input className="validate"
-                id="email"
-                name="email"
-                onChange={this.handleFieldChange}
-                required
-                type="text"
-            />
-          <label htmlFor="email">Email Address</label>
-          </div>
-          <div className="input-field col s12">
-            <input className="validate"
-                id="password"
-                name="password"
-                onChange={this.handleFieldChange}
-                required
-                type="password"
-            />
-          <label htmlFor="password">Password</label>
-          </div>
-          <div className="col s12">
-            <div className="container center">
-              <button className="btn waves-effect header-btn blue"
-                  name="action"
-                  type="submit"
-              > Login
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-    );
+    setupPorts = ports => {
+      let userStore = this.userStore;
+      // Receives a record from Elm, which comes in as a JS obect
+      ports.handleSubmit.subscribe(function(model) {
+        let loginPayload = {
+          username: model.email,
+          password: model.password
+        };
+        UserActions.login(loginPayload, userStore);
+      });
+    };
+
+    render() {
+      return <Elm src={Login} ports={this.setupPorts} />;
+    }
   }
-});
+);
 
 module.exports = LoginForm;
