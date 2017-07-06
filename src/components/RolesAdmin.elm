6f569db5526c8d4bd1ada9773b11449c558d8b7a
Kevin Ndung'u <kevgathuku@gmail.com>
2017-07-06 17:41:11 +0300

    Port RolesAdmin to Elm

---
+++ src/components/RolesAdmin.elm
@@ -0,0 +1,169 @@
+module RolesAdmin exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Http exposing (..)
+import Json.Decode as Decode exposing (int, field, string, map3)
+import HttpBuilder exposing (..)
+
+
+type alias Model =
+    { token : String
+    , baseURL : String
+    , roles : List Role
+    , rolesFetchError : String
+    }
+
+
+type alias Role =
+    { id_ : String
+    , title : String
+    , accessLevel : Int
+    }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+getWithToken : String -> String -> Request (List Role)
+getWithToken url token =
+    HttpBuilder.get url
+        |> withHeader "x-access-token" token
+        |> withExpect (Http.expectJson decodeRoles)
+        |> toRequest
+
+
+fetchRoles : String -> String -> Cmd Msg
+fetchRoles token baseURL =
+    let
+        url =
+            baseURL ++ "/api/roles"
+
+        request =
+            getWithToken url token
+    in
+        Http.send HandleRolesResult request
+
+
+roleDecoder : Decode.Decoder Role
+roleDecoder =
+    Decode.map3 Role
+        (field "_id" string)
+        (field "title" string)
+        (field "accessLevel" int)
+
+
+decodeRoles : Decode.Decoder (List Role)
+decodeRoles =
+    Decode.list roleDecoder
+
+
+httpErrorToString : Http.Error -> String
+httpErrorToString error =
+    case error of
+        BadUrl text ->
+            "Bad Url: " ++ text
+
+        Timeout ->
+            "Http Timeout"
+
+        NetworkError ->
+            "Network Error"
+
+        BadStatus response ->
+            "Bad Http Status: " ++ toString response.status.code
+
+        BadPayload message response ->
+            "Bad Http Payload: "
+                ++ toString message
+                ++ " ("
+                ++ toString response.status.code
+                ++ ")"
+
+
+type Msg
+    = FetchRoles
+    | HandleRolesResult (Result Http.Error (List Role))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        FetchRoles ->
+            ( model, Cmd.none )
+
+        HandleRolesResult (Ok roles) ->
+            ( { model | roles = roles }, Cmd.none )
+
+        HandleRolesResult (Err error) ->
+            ( { model | rolesFetchError = httpErrorToString error }, Cmd.none )
+
+
+intialModel : Model
+intialModel =
+    { token = ""
+    , baseURL = ""
+    , roles = []
+    , rolesFetchError = ""
+    }
+
+
+type alias Flags =
+    { token : String
+    , baseURL : String
+    }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( { intialModel | token = flags.token, baseURL = flags.baseURL }
+    , fetchRoles flags.token flags.baseURL
+    )
+
+
+main : Program Flags Model Msg
+main =
+    programWithFlags { init = init, view = view, update = update, subscriptions = subscriptions }
+
+
+renderRole : Role -> Html Msg
+renderRole role =
+    tr []
+        [ td [] [ text role.title ]
+        , td [] [ text (toString role.accessLevel) ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ div [ class "card-panel" ]
+            [ h2 [ class "header center-align" ] [ text "Manage Roles" ]
+            , div [ class "row" ]
+                [ div [ class "col s10 offset-s1 center-align" ]
+                    [ table [ class "centered" ]
+                        [ thead []
+                            [ tr []
+                                [ th [ attribute "data-field" "id" ] [ text "Role" ]
+                                , th [ attribute "data-field" "name" ] [ text "Access Level" ]
+                                ]
+                            ]
+                        , tbody [] (List.map renderRole model.roles)
+                        ]
+                    ]
+                ]
+            , div
+                [ class "fixed-action-btn"
+                , style
+                    [ ( "bottom", "45px" )
+                    , ( "right", "24px" )
+                    ]
+                ]
+                [ a [ class "btn-floating btn-large tooltipped pink", attribute "data-delay" "50", attribute "data-position" "left", attribute "data-tooltip" "Create Role", href "/admin/roles/create" ]
+                    [ i [ class "material-icons" ] [ text "edit" ]
+                    ]
+                ]
+            ]
+        ]
--- src/components/RolesAdmin/index.jsx
+++ src/components/RolesAdmin/index.jsx
@@ -1,72 +1,16 @@
 import React from 'react';
-import RoleActions from '../../actions/RoleActions';
-import RoleStore from '../../stores/RoleStore';
+import Elm from 'react-elm-components';
 
-class RolesAdmin extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      docs: null,
-      token: localStorage.getItem('user')
-    };
-  }
+import BaseActions from '../../actions/BaseActions';
+import { RolesAdmin } from '../RolesAdmin.elm';
 
-  componentDidMount() {
-    RoleActions.getRoles(this.state.token);
-    RoleStore.addChangeListener(this.handleRolesResult);
-  }
-
-  componentWillUnmount() {
-    RoleStore.removeChangeListener(this.handleRolesResult);
-  }
-
-  handleRolesResult = () => {
-    let roles = RoleStore.getRoles();
-    this.setState({roles: roles});
+export default class RolesAdminComponent extends React.Component {
+  flags = {
+    token: localStorage.getItem('user'),
+    baseURL: BaseActions.BASE_URL
   };
 
   render() {
-    let renderRole = (role) => {
-      return (
-        <tr key={role._id}>
-        <td>{role.title}</td>
-        <td>{role.accessLevel}</td>
-      </tr>
-    );
-    };
-    return (
-      <div className="container">
-        <div className="card-panel">
-          <h2 className="header center-align">Manage Roles</h2>
-            <div className="row">
-              <div className="col s10 offset-s1 center-align">
-                <table className="centered">
-                  <thead>
-                    <tr>
-                      <th data-field="id">Role</th>
-                      <th data-field="name">Access Level</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.state.roles ? this.state.roles.map(renderRole) : null}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-        </div>
-        <div className="fixed-action-btn" style={{bottom: 45, right: 24}}>
-          <a className="btn-floating btn-large tooltipped pink"
-              data-delay="50"
-              data-position="left"
-              data-tooltip="Create Role"
-              href="/admin/roles/create"
-          >
-            <i className="material-icons">edit</i>
-          </a>
-        </div>
-      </div>
-    );
+    return <Elm src={RolesAdmin} flags={this.flags} />;
   }
 }
-
-module.exports = RolesAdmin;
