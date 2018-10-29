565a34d33f24332bd9a647f51e1e07a79bea25d9
Kevin Ndung'u <kevgathuku@gmail.com>
2018-10-29 15:05:58 +0300

    Update Auth component tests

--- src/components/Auth/__tests__/Auth.test.js
+++ src/components/Auth/__tests__/Auth.test.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import expect from 'expect';
+import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
+
 import Auth from '../Auth';
-import Login from '../../Login/Login.jsx';
-import SignUp from '../../SignUp/SignUp.jsx';
 
 describe('Auth', function() {
-  describe('Component Rendering', function() {
-    it('displays the correct contents', function() {
-      // It should find the correct content
-      expect(shallow(<Auth />).text()).toMatch(/Login/);
-      expect(shallow(<Auth />).text()).toMatch(/Signup/);
-    });
+  const props = { history: {} };
 
-    it('renders the correct component', function() {
-      expect(shallow(<Auth />).is('.container')).toEqual(true);
-      expect(shallow(<Auth />).find('.card-panel').length).toEqual(1);
-    });
-
-    it('renders the children components', function() {
-      // It renders the provided child components
-      const auth = shallow(<Auth />);
-      expect(auth.find(Login)).toExist();
-      expect(auth.find(SignUp)).toExist();
-    });
+  it('renders correctly', function() {
+    const wrapper = shallow(<Auth {...props} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
