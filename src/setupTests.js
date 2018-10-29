c00818349ad99592fc8867d27551c268dd04b53d
Kevin Ndung'u <kevgathuku@gmail.com>
2018-10-29 13:34:18 +0300

    Configure enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,10 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 const sessionstorage = require('sessionstorage');
 const localStorage = require('localStorage');
 
 global.localStorage = localStorage;
 global.sessionStorage = sessionstorage;
+
+Enzyme.configure({ adapter: new Adapter() });
