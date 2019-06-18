ecf1d59774c93c9a8b90169e0225f3d1ac3da1be
Kevin Ndung'u <kevgathuku@gmail.com>
2019-06-18 12:08:07 +0300

    Update tests to match new elm-test version

--- tests/LandingTest.elm
+++ tests/LandingTest.elm
@@ -1,9 +1,10 @@
 module LandingTest exposing (suite)
 
+import Html.Attributes as Attr
 import Landing
 import Test exposing (..)
 import Test.Html.Query as Query
-import Test.Html.Selector exposing (attribute, text)
+import Test.Html.Selector exposing (attribute, tag, text)
 
 
 suite : Test
@@ -13,6 +14,6 @@ suite =
             \() ->
                 Landing.view { authLink = "/authenticate" }
                     |> Query.fromHtml
-                    |> Query.find [ attribute "href" "/authenticate" ]
+                    |> Query.find [ tag "a" ]
                     |> Query.has [ text "Get Started" ]
         ]
